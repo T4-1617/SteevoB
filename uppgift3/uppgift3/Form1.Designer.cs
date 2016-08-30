1310e11a423dfc1581ef461d9cfb69793c6202e9
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-08-30 13:14:37 +0200

    Code is done and layout is final

--- uppgift3/uppgift3/Form1.Designer.cs
+++ uppgift3/uppgift3/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace uppgift2
+﻿namespace uppgift3
 {
     partial class Form1
     {
@@ -28,67 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.nameBox1 = new System.Windows.Forms.TextBox();
-            this.nameBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.helloBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // nameBox1
+            // textBox1
             // 
-            this.nameBox1.Location = new System.Drawing.Point(78, 26);
-            this.nameBox1.Name = "nameBox1";
-            this.nameBox1.Size = new System.Drawing.Size(118, 20);
-            this.nameBox1.TabIndex = 0;
-            this.nameBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Location = new System.Drawing.Point(119, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 0;
             // 
-            // nameBox2
+            // textBox2
             // 
-            this.nameBox2.Location = new System.Drawing.Point(78, 73);
-            this.nameBox2.Name = "nameBox2";
-            this.nameBox2.Size = new System.Drawing.Size(118, 20);
-            this.nameBox2.TabIndex = 1;
-            this.nameBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Location = new System.Drawing.Point(119, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Location = new System.Drawing.Point(43, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 13);
-            this.label1.TabIndex = 2;
+            this.label1.TabIndex = 3;
             this.label1.Text = "Förnamn";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Location = new System.Drawing.Point(43, 116);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 13);
-            this.label2.TabIndex = 3;
+            this.label2.TabIndex = 4;
             this.label2.Text = "Efternamn";
-            // 
-            // helloBtn
-            // 
-            this.helloBtn.Location = new System.Drawing.Point(78, 162);
-            this.helloBtn.Name = "helloBtn";
-            this.helloBtn.Size = new System.Drawing.Size(71, 28);
-            this.helloBtn.TabIndex = 4;
-            this.helloBtn.Text = "Hello";
-            this.helloBtn.UseVisualStyleBackColor = true;
-            this.helloBtn.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(222, 248);
-            this.Controls.Add(this.helloBtn);
+            this.ClientSize = new System.Drawing.Size(402, 401);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.nameBox2);
-            this.Controls.Add(this.nameBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -98,11 +96,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox nameBox1;
-        private System.Windows.Forms.TextBox nameBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button helloBtn;
     }
 }
 
