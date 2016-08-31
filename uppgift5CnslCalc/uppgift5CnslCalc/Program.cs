7e70e6cf6d309b90e08a047237e560ec5e02e6e5
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-08-31 13:10:32 +0200

    Console calculator done

---
+++ uppgift5CnslCalc/uppgift5CnslCalc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift5CnslCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in tal 1:");
+            double value1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Skriv in tal 2:");
+            double value2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Välj räknesätt: (+ - * /)");
+            string raknesatt = Console.ReadLine();
+            double answer;
+            switch(raknesatt)
+            {
+                case "+":
+                    answer = value1 + value2;
+                    Console.WriteLine(answer);
+                    break;
+
+                case "-":
+                    answer = value1 - value2;
+                    Console.WriteLine(answer);
+                    break;
+
+                case "*":
+                    answer = value1 * value2;
+                    Console.WriteLine(answer);
+                    break;
+
+                case "/":
+                    answer = value1 / value2;
+                    Console.WriteLine(answer);
+                    break;
+
+            }
+            Console.WriteLine("Tryck Enter för att avsluta.");
+            Console.ReadLine();
+
+        }
+    }
+}
