b37645a69a2ae8ab3f096ef7aa88e079c48c02bf
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-08-31 12:45:43 +0200

    All is done

---
+++ uppgift4/uppgift4/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace uppgift4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(12, 195);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(49, 23);
+            this.btnPlus.TabIndex = 2;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(67, 195);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(49, 23);
+            this.btnMin.TabIndex = 3;
+            this.btnMin.Text = "-";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(122, 195);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(49, 23);
+            this.btnMult.TabIndex = 4;
+            this.btnMult.Text = "x";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(177, 195);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(49, 23);
+            this.btnDiv.TabIndex = 5;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tal 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Tal 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 256);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ uppgift4/uppgift4/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppgift4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox3.Text = String.Empty;
+
+            double value1;
+            value1 = Convert.ToDouble(textBox1.Text);
+
+            double value2;
+            value2 = Convert.ToDouble(textBox2.Text);
+
+            double answerDbl = value1 + value2;
+            string answer;
+            answer = answerDbl.ToString();
+
+            textBox3.AppendText(answer);
+
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = String.Empty;
+
+            double value1;
+            value1 = Convert.ToDouble(textBox1.Text);
+
+            double value2;
+            value2 = Convert.ToDouble(textBox2.Text);
+
+            double answerDbl = value1 - value2;
+            string answer;
+            answer = answerDbl.ToString();
+
+            textBox3.AppendText(answer);
+
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = String.Empty;
+
+            double value1;
+            value1 = Convert.ToDouble(textBox1.Text);
+
+            double value2;
+            value2 = Convert.ToDouble(textBox2.Text);
+
+            double answerDbl = value1 * value2;
+            string answer;
+            answer = answerDbl.ToString();
+
+            textBox3.AppendText(answer);
+
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = String.Empty;
+
+            double value1;
+            value1 = Convert.ToDouble(textBox1.Text);
+
+            double value2;
+            value2 = Convert.ToDouble(textBox2.Text);
+
+            double answerDbl = value1 / value2;
+            string answer;
+            answer = answerDbl.ToString();
+
+            textBox3.AppendText(answer);
+
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
