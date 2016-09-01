b79476a72a687bf9006e99187c644e69f9ee0be5
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-09-01 11:59:35 +0200

    Initial commit - Writes name length

---
+++ String010916/String010916/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String010916
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your full name.");
+            string name;
+            name = Console.ReadLine();
+
+
+            Console.WriteLine(name.Length);
+
+
+        }
+    }
+}
