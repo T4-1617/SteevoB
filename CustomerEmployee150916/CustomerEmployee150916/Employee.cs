c8f383342d8cc497a7bf9afd68841a3d150653b1
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-09-15 16:30:24 +0200

    Created classes

---
+++ CustomerEmployee150916/CustomerEmployee150916/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerEmployee150916
+{
+    class Employee
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{1}, {0}", FirstName, LastName);
+            }
+        }
+    }
+}
