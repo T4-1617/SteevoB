778c98516fa50a29d7296f2b8e1ae5d6b04ac25b
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-09-16 11:53:31 +0200

    Initial commit, layout done

---
+++ ACMECAR160916/ACMECAR160916/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMECAR160916
+{
+    class Car
+    {
+        public string MyProperty { get; set; }
+    }
+}
